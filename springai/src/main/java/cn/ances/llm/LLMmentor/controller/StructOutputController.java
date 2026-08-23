98d552524dac1ad42ad57afe6074a7d0aab75af0
ances <tao.best@foxmail.com>
2026-08-23 18:35:05 +0800

    Sring AI结构化输出--list、Map

--- springai/src/main/java/cn/ances/llm/LLMmentor/controller/StructOutputController.java
+++ springai/src/main/java/cn/ances/llm/LLMmentor/controller/StructOutputController.java
@@ -10,11 +10,15 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,7 +32,7 @@ public class StructOutputController implements InitializingBean {
     private ChatClient chatClient;
 
     @Override
-    public void afterPropertiesSet() throws Exception{
+    public void afterPropertiesSet() throws Exception {
         chatClient = ChatClient.builder(chatModel)
                 .defaultOptions(DashScopeChatOptions.builder()
                         .temperature(0.7)
@@ -39,7 +43,7 @@ public class StructOutputController implements InitializingBean {
     }
 
     @RequestMapping("/call")
-    public String call(String message){
+    public String call(String message) {
 
         PromptTemplate promptTemplate = PromptTemplate.builder().template("请给我推荐基本心理学有关的书，输出格式：{format}").build();
 
@@ -47,7 +51,7 @@ public class StructOutputController implements InitializingBean {
 
         //.content()
         //.chatResponse().getResult().getOutput().getText()
-        String resp = chatClient.prompt(promptTemplate.create(Map.of("format",converter.getFormat())))
+        String resp = chatClient.prompt(promptTemplate.create(Map.of("format", converter.getFormat())))
                 .call().chatResponse().getResult().getOutput().getText();
 
         BookRecord book = converter.convert(resp);
@@ -57,7 +61,7 @@ public class StructOutputController implements InitializingBean {
     }
 
     @RequestMapping("/convert")
-    public String convert(){
+    public String convert() {
         //提示词 + 格式，非常简洁。
         // entity方法
         BookRecord book = chatClient.prompt("请给我推荐基本心理学有关的书")
@@ -65,5 +69,29 @@ public class StructOutputController implements InitializingBean {
         log.info(book.toString());
 
         return book.title() + " - " + book.author() + " - " + book.desc() + " - " + book.publisher() + " - " + book.price();
+    }
+
+    //实际上还是BeanOutputConverter
+    @RequestMapping("/convertList")
+    public String convertList() {
+        // entity方法
+        List<BookRecord> bookList = chatClient.prompt("请给我推荐基本心理学有关的书")
+                .call().entity(new ParameterizedTypeReference<List<BookRecord>>() {
+                });
+        log.info(bookList.toString());
+
+        return bookList.toString();
+    }
+
+    //只能返回Map，但是没有按照要求返回--通过提示词语言描述来达到目的
+    //一般都是拿list通过stream转换为map
+    @RequestMapping("/convertMap")
+    public String convertMap() {
+        // entity方法
+        Map<String, Object> bookRecordMap = chatClient.prompt("请给我推荐几本心理学有关的书，书的内容包括书名、作者、价格、上市时间等信息，以书名作为key，书的信息作为value")
+                .call().entity(new MapOutputConverter());
+        log.info(bookRecordMap.toString());
+
+        return bookRecordMap.toString();
     }
 }
